04d4cfb4129788cdb76289c2f56d9d9b34098128
GloireM05 <101839712+GloireM05@users.noreply.github.com>
2022-03-21 20:21:40 +0100

    Update unirest.js

--- unirest.js
+++ unirest.js
@@ -1,9 +1,9 @@
-var unirest = require("unirest")
-var req = unirest("GET", "https://my-json-server.typicode.com/GloireM05/alos_tp1/db?$top=10")
+var unirest = require("unirest");
+var req = unirest("GET", "https://my-json-server.typicode.com/GloireM05/alos_tp1/db?$top=10");
 req.headers({ 
 			"cache-control": "no-cache"
-})
+});
 req.end(function (res) { 
 	if(res.error) throw new Error(res.error) 
 		console.log(res.body)
-})
+});
